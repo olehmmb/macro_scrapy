ee421d454df4123f1a8ede44edf46e2c801ef5fd
luftuq <qwe454jkl@gmail.com>
2024-03-01 14:24:15 +0100

    scrapy-playwright integration

--- macro_scrapy/macro_scrapy/items.py
+++ macro_scrapy/macro_scrapy/items.py
@@ -9,6 +9,11 @@ import scrapy
 class MacroScrapyItem(scrapy.Item):
     # define the fields for your item here like:
     file_urls = scrapy.Field()
-    original_file_name = scrapy.Field()  
+    original_file_name = scrapy.Field()
     files = scrapy.Field
 
+class QuoteItem(scrapy.Item):
+    # define the fields for your item here like:
+    text = scrapy.Field()
+    author = scrapy.Field()
+    tags = scrapy.Field()
--- macro_scrapy/macro_scrapy/settings.py
+++ macro_scrapy/macro_scrapy/settings.py
@@ -91,3 +91,10 @@ FILES_STORE = r"/workspaces/macro_scrapy/downloaded"
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+DOWNLOAD_HANDLERS = {
+    "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+    "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+}
+PLAYWRIGHT_LAUNCH_OPTIONS = {
+        "headless": True,
+} 
