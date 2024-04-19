3c2648650000484afec8072bd18213bb28e71671
simon-dlouhy <tortensen@gmail.com>
2024-04-19 15:40:54 +0200

    household debt done

--- macro_scrapy/macro_scrapy/processing/household_debt.py
+++ macro_scrapy/macro_scrapy/processing/household_debt.py
@@ -1,23 +1,20 @@
-from pathlib import Path
-
 import polars as pl
 from __init__ import folder_name, parent_folder, yearly_data
 
 
-def calculate_v4(df):
+def calculate_v4(df: pl.DataFrame) -> pl.Series:
     pl_series = df.filter(pl.col("LOCATION") == "POL")[["Value"]]
     hu_series = df.filter(pl.col("LOCATION") == "HUN")[["Value"]]
     svk_series = df.filter(pl.col("LOCATION") == "SVK")[["Value"]]
     v4_df = pl.DataFrame({"poland":pl_series, "hungary":hu_series, "slovakia":svk_series})
-    v4_series = ((v4_df.with_columns(mean=pl.mean_horizontal("poland", "hungary", "slovakia")))["mean"])
-    return(v4_series)
+    return ((v4_df.with_columns(mean=pl.mean_horizontal("poland", "hungary", "slovakia")))["mean"])
 
-def calculate_household_debt(df):
+def calculate_household_debt(df: pl.DataFrame) -> pl.DataFrame:
     cze_series = df.filter(pl.col("LOCATION") == "CZE")[["Value"]]
     de_series = df.filter(pl.col("LOCATION") == "DEU")[["Value"]]
     years = yearly_data(len(cze_series)-1)
-    df = pl.DataFrame({"Year":years, "Czechia":cze_series, "Germany":de_series, "V4":calculate_v4(df)})
-    df.write_excel(fr"{parent_folder}\data\{folder_name}\output\{folder_name}_HouseholdsDebt.xlsx", worksheet = "HouseholdsDebt")
+    final_df = pl.DataFrame({"Time":years, "Czechia":cze_series, "Germany":de_series, "V4":calculate_v4(df)})
+    final_df.write_excel(fr"{parent_folder}\data\{folder_name}\output\{folder_name}_HouseholdsDebt.xlsx", worksheet = "HouseholdsDebt")
 
-df = pl.read_csv(fr"{parent_folder}\data\{folder_name}\input\{folder_name}_Households_Debt.csv")
-calculate_household_debt(df)
+final_df = pl.read_csv(fr"{parent_folder}\data\{folder_name}\input\{folder_name}_Households_Debt.csv")
+calculate_household_debt(final_df)
