a31ad8fa72d6f4e57ce1b339995d053ad039cb82
luftuq <qwe454jkl@gmail.com>
2024-04-25 17:03:31 +0200

    emplyment_potential simplification

--- macro_scrapy/macro_scrapy/processing/employment_potential.py
+++ macro_scrapy/macro_scrapy/processing/employment_potential.py
@@ -1,21 +1,9 @@
 import polars as pl
 from __init__ import folder_name, parent_folder
 
-emppot_df = pl.read_excel(fr"{parent_folder}\data\{folder_name}\input\{folder_name}_Sentiment.xlsx", sheet_name="Data")
+employee_potential = pl.read_excel(fr"{parent_folder}/data/{folder_name}/input/{folder_name}_Sentiment.xlsx", sheet_name="Data",engine = "calamine", read_options = {"header_row": 1,"use_columns":[0,1,3]})
+employee_potential.columns = ["Year", "Month", "Shortage of Employees"]
+employee_potential = employee_potential.with_columns(pl.col("Year").forward_fill())  
+employee_potential.write_excel(fr"{parent_folder}/data/{folder_name}/output/{folder_name}_EmploymentPotential.xlsx", worksheet = "EmploymentPotential")
 
-def emppottransformation(df: pl.DataFrame) -> pl.DataFrame:
-    intial_df = pl.DataFrame(df)
-    trimmed_df = intial_df.select("Bariéry průmysl", "_duplicated_2")
-    trimmed_df = trimmed_df.slice(1,len(trimmed_df))
 
-    num_years = len(trimmed_df) // 4
-    last_year_rows = len(trimmed_df) % 4
-
-    years = pl.Series([year for year in range(2005, 2005 + num_years) for i in range(4)])
-    years.extend(pl.Series([2005 + num_years] * last_year_rows))
-
-    df_final = trimmed_df.with_columns(year=years)
-    df_final.columns = ["Month", "Shortage of Employees", "Year"]
-    df_final.write_excel(fr"{parent_folder}\data\{folder_name}\output\{folder_name}_EmploymentPotential.xlsx", worksheet = "EmploymentPotential")
-
-emppottransformation(emppot_df)
