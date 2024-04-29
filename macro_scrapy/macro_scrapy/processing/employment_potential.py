0f2b00ed1d892c75f36afa06acf87be469cb8d4c
luftuq <qwe454jkl@gmail.com>
2024-04-29 16:20:30 +0200

    employment_potential formated

--- macro_scrapy/macro_scrapy/processing/employment_potential.py
+++ macro_scrapy/macro_scrapy/processing/employment_potential.py
@@ -1,9 +1,18 @@
+"""Output for Employment potential calculation."""
 import polars as pl
-from __init__ import folder_name, parent_folder
+from __init__ import input_path, output_path
 
-employee_potential = pl.read_excel(fr"{parent_folder}/data/{folder_name}/input/{folder_name}_Sentiment.xlsx", sheet_name="Data",engine = "calamine", read_options = {"header_row": 1,"use_columns":[0,1,3]})
-employee_potential.columns = ["Year", "Month", "Shortage of Employees"]
-employee_potential = employee_potential.with_columns(pl.col("Year").forward_fill())  
-employee_potential.write_excel(fr"{parent_folder}/data/{folder_name}/output/{folder_name}_EmploymentPotential.xlsx", worksheet = "EmploymentPotential")
-
-
+employee_potential = pl.read_excel(
+    '{0}_Sentiment.xlsx'.format(input_path),
+    sheet_name='Data',
+    engine='calamine',
+    read_options={'header_row': 1, 'use_columns': [0, 1, 3]},
+    )
+employee_potential.columns = ['Year', 'Month', 'Shortage of Employees']
+employee_potential = employee_potential.with_columns(
+    pl.col('Year').forward_fill(),
+    )
+employee_potential.write_excel(
+    '{0}_EmploymentPotential.xlsx'.format(output_path),
+    worksheet='EmploymentPotential',
+    )
