9af3dfa8344ae5da15257c43fee342d585877d19
luftuq <qwe454jkl@gmail.com>
2024-05-06 14:33:28 +0200

    ExcelHandler class added and applied in employment_potential.py and unemployment_mpsv.py

--- macro_scrapy/macro_scrapy/processing/__init__.py
+++ macro_scrapy/macro_scrapy/processing/__init__.py
@@ -1,23 +1,27 @@
 """Processing management package.
 
 This package provides resources for processing.
-It introduces variables that are subsequently used for obtaining / storing raw and processed data.
+It introduces variables that are subsequently used.
 """
 from datetime import datetime as dt
 from datetime import timezone
 
+import re
 import polars as pl
 from xls2xlsx import XLS2XLSX
+from zipfile import ZipFile
 
-parent_folder = r"/workspaces/macro_scrapy"
+parent_folder = r'/workspaces/macro_scrapy'
 current_date = dt.now(tz=timezone.utc)
 current_year = current_date.year
-folder_name = current_date.strftime("%Y%m%d")
-input_path = parent_folder+"/data/"+folder_name+"/input/"+folder_name
-output_path = parent_folder+"/data/"+folder_name+"/output/"+folder_name
+folder_name = current_date.strftime('%Y%m%d')
+input_path = parent_folder+'/data/'+folder_name+'/input/'+folder_name+'_'
+output_path = parent_folder+'/data/'+folder_name+'/output/'+folder_name+'_'
 quarter_months = [0, 3, 6, 9]
-months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-date_list =  []
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+          'August', 'September', 'October', 'November', 'December']
+date_list = []
+
 
 def monthly_data(years: int) -> list:
     for i in range(years, -1, -1):
@@ -25,36 +29,39 @@ def monthly_data(years: int) -> list:
         for j in range(0, len(months), 3):
             date_list.extend(months[j:j+3])
             if j == quarter_months[0]:
-                date_list.append("Q1")
+                date_list.append('Q1')
             elif j == quarter_months[1]:
-                date_list.append("Q2")
+                date_list.append('Q2')
             elif j == quarter_months[2]:
-                date_list.append("Q3")
+                date_list.append('Q3')
             elif j == quarter_months[3]:
-                date_list.append("Q4")
+                date_list.append('Q4')
         date_list.append(specific_year)
     return date_list
+
 
 def quarterly_data(years: int) -> list:
     for i in range(years, -1, -1):
         for j in range(0, len(months), 3):
             if j == quarter_months[0]:
-                date_list.append("Q1")
+                date_list.append('Q1')
             elif j == quarter_months[1]:
-                date_list.append("Q2")
+                date_list.append('Q2')
             elif j == quarter_months[2]:
-                date_list.append("Q3")
+                date_list.append('Q3')
             elif j == quarter_months[3]:
-                date_list.append("Q4")
+                date_list.append('Q4')
         specific_year = str(current_year - i)
         date_list.append(specific_year)
     return date_list
+
 
 def yearly_data(years: int) -> list:
     for i in range(years, -1, -1):
         specific_year = str(current_year - i)
         date_list.append(specific_year)
     return date_list
+
 
 def monthly_data_no_qy(years: int) -> list:
     for i in range(years, -1, -1):
@@ -64,9 +71,11 @@ def monthly_data_no_qy(years: int) -> list:
         date_list.append(specific_year)
     return date_list
 
+
 def convert_xls(file_to_convert: any) -> any:
     convertable = XLS2XLSX(file_to_convert)
     return convertable.to_xlsx()
+
 
 def average_every_4(lst: any) -> list:
     new_list = []
@@ -76,6 +85,7 @@ def average_every_4(lst: any) -> list:
         new_list.extend(group)
         new_list.append(group_average)
     return new_list
+
 
 def combine_lists_monthly(m: list, q: list, y: list) -> list:
     final_list = []
@@ -91,13 +101,16 @@ def combine_lists_monthly(m: list, q: list, y: list) -> list:
             index_y += 1
     return final_list
 
+
 def row_selection(df_r: pl.DataFrame, row: int, column: any) -> any:
     df_r = df_r[row, column:]
     return df_r.transpose(include_header=False)
 
+
 def column_selection(df_c: pl.DataFrame, row: any, column: int) -> pl.DataFrame:
     df_c = df_c[row:, column]
     return pl.DataFrame(df_c)
+
 
 def list_growth(sequence: any) -> list:
     growth_list = []
@@ -105,3 +118,80 @@ def list_growth(sequence: any) -> list:
         growth = i-100
         growth_list.extend(growth)
     return growth_list
+
+
+class ExcelHandler:
+    """A class to handle Excel files within a zip."""
+
+    def __init__(self):
+        """Initialize ExcelHandler with necessary attributes."""
+        self.excel_file_name = None
+        self.excel_stream = None
+
+    def find_excel_file_in_zip(self, input_file: str, file_regex: str):
+        """
+        Find the first Excel file in zip that matches a regular expression.
+
+        Args:
+            input_file (str): The input zip file.
+            file_regex (str): The regular expression to match the Excel file.
+
+        Returns:
+            ExcelHandler: An instance of the ExcelHandler with the found Excel.
+        """
+        self.input_file = input_file
+        self.file_regex = file_regex
+        with ZipFile(self.input_file, 'r') as zip:
+            for name in zip.namelist():
+                if re.match(self.file_regex, name):
+                    self.excel_file_name = name
+                    break
+        return self
+
+    def unzip_file(self) -> 'ExcelHandler':
+        """
+        Extract specified file from zip archive.
+
+        Returns:
+            ExcelHandler: An instance of the ExcelHandler class.
+        """
+        with ZipFile(self.input_file, 'r') as zip:
+            self.excel_stream = zip.open(self.excel_file_name)
+        return self
+
+    def read_data(self, excel_stream=None, sheet_name='Sheet1', skip_rows=0,
+                  has_header=True, columns=None,
+                  ) -> 'ExcelHandler':
+        """
+        Read an Excel file into a DataFrame.
+
+        Args:
+            excel_stream (str, optional): The input Excel stream. Defaults to instance's excel_stream.
+            sheet_name (str, optional): The name of the sheet to read from. Defaults to 'Sheet1'.
+            skip_rows (int, optional): The number of rows to skip at the beginning. Defaults to 0.
+            has_header (bool, optional): Whether the Excel file has a header. Defaults to True.
+            columns (list, optional): The list of column names to consider. If not provided, all columns are considered.
+
+        Returns:
+            ExcelHandler: An instance of the ExcelHandler class with the DataFrame read from the Excel file.
+        """
+        if excel_stream is None:
+            excel_stream = self.excel_stream
+        self.df = pl.read_excel(
+            excel_stream,
+            sheet_name=sheet_name,
+            read_options={'skip_rows': skip_rows, 'has_header': has_header,
+                          'columns': columns,
+                          },
+        )
+        return self
+
+    def write_data(self, output_file: str) -> 'ExcelHandler':
+        """
+        Write DataFrame to Excel file.
+
+        Returns:
+            ExcelHandler: An instance of the ExcelHandler class.
+        """
+        self.df.write_excel(output_file)
+        return self
--- macro_scrapy/macro_scrapy/processing/unemployment_mpsv.py
+++ macro_scrapy/macro_scrapy/processing/unemployment_mpsv.py
@@ -1,101 +1,43 @@
 """Main module for processing unemployment data."""
 from datetime import date
-from zipfile import ZipFile
 
 import polars as pl
-from __init__ import input_path, output_path
+from __init__ import ExcelHandler, input_path, output_path
 
 
 class UnemploymentData:
-    """
-    A class to process unemployment data from an Excel file within a zip.
+    """A class to process unemployment data from an Excel file within a zip."""
 
-    Attributes:
-        start_date (date): Start date for the data series.
-        input_zip (str): Input zip file name without extension.
-        output_file (str): Output Excel file name without extension.
-    """
+    def __init__(self):
+        """Initialize UnemploymentData with necessary attributes."""
+        self.start_date = date(1991, 1, 31)
+        self.output_file = '{0}UnemployedVacancies.xlsx'.format(output_path)
+        self.input_file = '{0}Unemployment.zip'.format(input_path)
+        self.pattern = r'^2\. Nez.*\.xlsx$'
+        self.excel_handler = ExcelHandler()
 
-    def __init__(self, start_date, input_zip, output_file):
-        """
-        Initialize UnemploymentData with necessary attributes.
-
-        Parameters:
-            start_date (date): Start date for the data series.
-            input_zip (str): Input zip file name without extension.
-            output_file (str): Output Excel file name without extension.
-        """
-        self.start_date = start_date
-        self.input_zip_path = '{0}_{1}'.format(input_path, input_zip)
-        self.output_file_path = '{0}_{1}'.format(output_path, output_file)
-
-    def find_excel_file_in_zip(self):
-        """
-        Find first Excel file in zip matching '2. Nez' and ending with '.xlsx'.
-
-        Returns:
-            str: Name of found Excel file or None if no match.
-        """
-        # Find first file matching '2. Nez' and ending with '.xlsx'
-        with ZipFile(self.input_zip_path, 'r') as zip_archive:
-            for name in zip_archive.namelist():
-                if name.startswith('2. Nez') and name.endswith('.xlsx'):
-                    return name
-        return None
-
-    def unzip_file(self, excel_file_name):
-        """
-        Extract specified file from zip archive.
-
-        Parameters:
-            excel_file_name (str): File name to extract.
-
-        Returns:
-            ZipExtFile: Extracted file object.
-        """
-        with ZipFile(self.input_zip_path, 'r') as zip_archive:
-            return zip_archive.open(excel_file_name)
-
-    def read_excel_file(self, excel_stream):
-        """
-        Read Excel file into DataFrame.
-
-        Parameters:
-            excel_stream (ZipExtFile): Excel file to read.
-
-        Returns:
-            DataFrame: Data as DataFrame.
-        """
-        return pl.read_excel(
-            excel_stream,
-            sheet_name='List1',
-            read_options={'skip_rows': 1, 'has_header': False},
-        )
-
-    def process_data(self, dataframe):
+    def process_data(self) -> 'UnemploymentData':
         """
         Process data by transposing and taking first two rows.
 
-        Parameters:
-            dataframe (DataFrame): Data to process.
-
         Returns:
-            DataFrame: Processed data.
+            UnemploymentData: An instance of the UnemploymentData class.
         """
-        return dataframe.head(2).transpose(column_names='column_1')
+        self.excel_handler.df = self.excel_handler.df.head(2)
+        self.excel_handler.df = self.excel_handler.df.transpose(
+            column_names='column_1',
+            )
+        return self
 
-    def add_date_column(self, dataframe):
+    def add_date_column(self) -> 'UnemploymentData':
         """
         Add date column to DataFrame based on start_date.
 
-        Parameters:
-            dataframe (DataFrame): Data to add date column to.
-
         Returns:
-            DataFrame: Data with added date column.
+            UnemploymentData: An instance of the UnemploymentData class.
         """
-        df_length = dataframe.shape[0] - 1
-        return dataframe.with_columns(
+        df_length = self.excel_handler.df.shape[0] - 1
+        self.excel_handler.df = self.excel_handler.df.with_columns(
             date=pl.date_range(
                 self.start_date,
                 pl.Series([self.start_date]).dt.offset_by(
@@ -105,27 +47,27 @@ class UnemploymentData:
                 eager=True,
             ),
         )
+        return self
 
-    def write_data(self, dataframe):
+    def run_it_all(self):
         """
-        Write DataFrame to Excel file.
+        Execute all the steps to process the unemployment data.
 
-        Parameters:
-            dataframe (DataFrame): Data to write to Excel file.
+        This method performs the following steps:
+        Finds the Excel file in the zip archiveExtracts the specified file.
+        Reads the Excel file into a DataFrame and processes the data by
+        transposing and taking the first two rows. Adds a date column to
+        the DataFrame. Writes the DataFrame to an Excel file.
         """
-        dataframe.write_excel(self.output_file_path)
+        self.excel_handler.find_excel_file_in_zip(
+            self.input_file, self.pattern,
+            ).unzip_file()
+        self.excel_handler.read_data(
+            sheet_name='List1', skip_rows=1, has_header=False,
+            )
+        self.process_data().add_date_column()
+        self.excel_handler.write_data(self.output_file)
 
 
 if __name__ == '__main__':
-    series_start = date(1991, 1, 31)
-    unemployment_data = UnemploymentData(
-        start_date=series_start,
-        input_zip='Unemployment.zip',
-        output_file='UnemployedVacancies.xlsx',
-    )
-    excel_file_name = unemployment_data.find_excel_file_in_zip()
-    excel_stream = unemployment_data.unzip_file(excel_file_name)
-    raw_df = unemployment_data.read_excel_file(excel_stream)
-    processed_df = unemployment_data.process_data(raw_df)
-    dataframe_with_dates = unemployment_data.add_date_column(processed_df)
-    unemployment_data.write_data(dataframe_with_dates)
+    UnemploymentData().run_it_all()
