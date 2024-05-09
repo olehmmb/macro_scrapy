0575204049f9328f312efd70f7f24f7aee384009
olehmmb <olehkurinny@gmail.com>
2024-05-09 17:10:46 +0200

    CsvHandler class has been added to handle reading data from .csv files. Imports and some docstrings were rearranged/edited for enhanced clarity.

--- macro_scrapy/macro_scrapy/processing/__init__.py
+++ macro_scrapy/macro_scrapy/processing/__init__.py
@@ -3,13 +3,13 @@
 This package provides resources for processing.
 It introduces variables that are subsequently used.
 """
+import re
 from datetime import datetime as dt
 from datetime import timezone
+from zipfile import ZipFile
 
-import re
 import polars as pl
 from xls2xlsx import XLS2XLSX
-from zipfile import ZipFile
 
 parent_folder = r'/workspaces/macro_scrapy'
 current_date = dt.now(tz=timezone.utc)
@@ -134,9 +134,10 @@ class ExcelHandler:
 
         Args:
             input_file (str): The input zip file.
-            file_regex (str): The regular expression to match the Excel file.
+            # file_regex (str): The regular expression to match the Excel file.
 
-        Returns:
+        Returns
+        -------
             ExcelHandler: An instance of the ExcelHandler with the found Excel.
         """
         self.input_file = input_file
@@ -152,7 +153,8 @@ class ExcelHandler:
         """
         Extract specified file from zip archive.
 
-        Returns:
+        Returns
+        -------
             ExcelHandler: An instance of the ExcelHandler class.
         """
         with ZipFile(self.input_file, 'r') as zip:
@@ -160,7 +162,7 @@ class ExcelHandler:
         return self
 
     def read_data(self, excel_stream=None, sheet_name='Sheet1', skip_rows=0,
-                  has_header=True, columns=None,
+                  has_header=True, columns=None, new_columns=None
                   ) -> 'ExcelHandler':
         """
         Read an Excel file into a DataFrame.
@@ -171,8 +173,10 @@ class ExcelHandler:
             skip_rows (int, optional): The number of rows to skip at the beginning. Defaults to 0.
             has_header (bool, optional): Whether the Excel file has a header. Defaults to True.
             columns (list, optional): The list of column names to consider. If not provided, all columns are considered.
+            new_columns (list, optional): The list of renamed (new) columns to be assigned instead of the original ones.
 
-        Returns:
+        Returns
+        -------
             ExcelHandler: An instance of the ExcelHandler class with the DataFrame read from the Excel file.
         """
         if excel_stream is None:
@@ -181,7 +185,7 @@ class ExcelHandler:
             excel_stream,
             sheet_name=sheet_name,
             read_options={'skip_rows': skip_rows, 'has_header': has_header,
-                          'columns': columns,
+                          'columns': columns, 'new_columns': new_columns
                           },
         )
         return self
@@ -190,8 +194,51 @@ class ExcelHandler:
         """
         Write DataFrame to Excel file.
 
-        Returns:
+        Returns
+        -------
             ExcelHandler: An instance of the ExcelHandler class.
+        """
+        self.df.write_excel(output_file)
+        return self
+
+class CsvHandler:
+    """A class to handle Csv files."""
+
+    def read_data(self, source, skip_rows=0,  # noqa: PLR0913
+                    has_header=False, separator=',',
+                    encoding='utf8', missing_utf8_is_empty_string=False
+                  ) -> 'CsvHandler':
+        """
+        Read an Csv file into a DataFrame.
+
+        Args:
+            source (str, optional): The input .csv file.
+            skip_rows (int, optional): The number of rows to skip at the beginning. Defaults to 0.
+            has_header (bool, optional): Whether the .csv file has a header. Defaults to True.
+            encoding (str, optional): encoding of the .csv file. Defaults to 'utf8'
+            missing_utf8_is_empty_string (bool, optional): whether to treat missing utf8 values as empty strings. Defaults to False.
+
+        Returns
+        -------
+            CsvHandler: An instance of the CsvHandler class with the DataFrame read from the .csv file.
+        """
+        if source is None:
+            source = self.source
+        self.df = pl.scan_csv(
+            source, has_header=has_header,
+            separator=separator, encoding=encoding,
+            missing_utf8_is_empty_string=missing_utf8_is_empty_string,
+            skip_rows=skip_rows
+        ).collect()
+        return self
+
+    def write_data(self, output_file: str) -> 'CsvHandler':
+        """
+        Write DataFrame to Excel file.
+
+        Returns
+        -------
+            ExcelHandler: An instance of the CsvHandler class.
         """
         self.df.write_excel(output_file)
         return self
