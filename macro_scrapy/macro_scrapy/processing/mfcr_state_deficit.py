567a25be9b91448f8f7d6e6286b42ccbb63d010c
olehmmb <olehkurinny@gmail.com>
2024-05-17 09:07:42 +0000

    Simplifying mfcr_state_deficit.py processing file to match new data scraped by mfcr_spider.py.

--- macro_scrapy/macro_scrapy/processing/mfcr_state_deficit.py
+++ macro_scrapy/macro_scrapy/processing/mfcr_state_deficit.py
@@ -1,47 +1,43 @@
 """Main module for processing MFCR state deficit data for years 2018--now()."""
 import polars as pl
-import os
-from __init__ import ExcelHandler, input_path, output_path, folder_name
-from pathlib import Path
+from __init__ import ExcelHandler, input_path, output_path
 
 
 class MFCRStateDeficitData:
-    """A class to process MFCR state deficit data exchange rates from a list...
-
-    of .csv files.
-    """
+    """A class to process MFCR state deficit data exchange rates from a csv."""
 
     def __init__(self):
         """Initialize MFCRStateDeficitData with necessary attributes."""
-        self.data_location = os.listdir(
-            '/workspaces/macro_scrapy/data/{0}/input'.format(folder_name),
-        )
-        self.state_deficit_final = pl.DataFrame()
+        self.input_file = '{0}MFCR_state_deficit.csv'.format(input_path)
         self.output_file = '{0}MFCR_State_Deficit.xlsx'.format(output_path)
         self.excel_handler = ExcelHandler()
 
     def rename_and_drop_columns(self) -> 'MFCRStateDeficitData':
-        """Rename columns and drop 'Příjmy' and 'Výdaje'.
+        """Drop 'Příjmy' and 'Výdaje' and rename columns.
 
         Returns:
             MFCRStateDeficitData: An instance of the MFCRStateDeficitData
             class.
         """
-        self.excel_handler.df.columns = [
-            'month', 'Příjmy', 'Výdaje', 'saldo', 'datum',
-        ]
         self.excel_handler.df = self.excel_handler.df.drop(
             ['Příjmy', 'Výdaje'],
+        ).rename(
+            {'2024': 'year',
+             'Tisková  zpráva': 'date_published',
+             'Měsíc': 'month',
+             'Saldo': 'saldo',
+             },
         )
+
         return self
 
     def clean_data(self) -> 'MFCRStateDeficitData':
         """Clean up the current dataframe.
 
-        Replace decimal comma with decimal dot.
         Remove invisible UTF characters (non-breaking spaces).
         Change the data type of column 'saldo' to Float64.
         Drop all rows that have a 'NA' in 'saldo' column.
+        Convert 'date_published' to Datetime.
 
         Returns:
             MFCRStateDeficitData: An instance of the MFCRStateDeficitData
@@ -51,105 +47,64 @@ class MFCRStateDeficitData:
             pl.col(
                 ['saldo'],
             ).str.replace(
-                ',', '.', literal=True,
-            ).str.replace(
                 '\xa0', '', literal=True,
             ).cast(
                 pl.Float64, strict=False,
             )).drop_nulls(
                 subset=['saldo'],
+            ).with_columns(
+                pl.col('date_published').str.to_datetime(
+                '%d.%m.%Y', strict=False,
+                ),
             )
         )
         return self
 
-    def get_last_observation_and_stack_it(self) -> 'MFCRStateDeficitData':
+    def leave_last_rows(self) -> 'MFCRStateDeficitData':
         """Get the last observation of the dataframe.
 
-        Get rid of first n-1 rows.
-        Convert 'datum' from String to Datetime.
-        Add the current dataframe (nrow=1) to
-        self.state_deficit_final.
+        Aggregate by year and get the last observation.
+        Rearrange columns.
 
         Returns:
             MFCRStateDeficitData: An instance of the MFCRStateDeficitData
             class.
         """
-        self.excel_handler.df = (self.excel_handler.df.slice(
-            self.excel_handler.df.height - 1,
-        ).with_columns(
-            pl.col('datum').str.to_datetime('%d.%m.%Y', strict=False),
-            )
+        self.excel_handler.df = (self.excel_handler.df.group_by(
+            'year',
+        ).agg(
+            pl.all().last(),
+        ).select(
+            'date_published', 'year', 'month', 'saldo',
+        ).sort(
+            'date_published',
         )
-        self.state_deficit_final = self.state_deficit_final.vstack(
-            self.excel_handler.df, in_place=True,
         )
-        return self
-
-    def process_all_csvs(self) -> 'MFCRStateDeficitData':
-        """Extract data from all .csv files of type 'MFCR_state_deficit'.
-
-        Then process each dataframe using previous methods.
-        """
-        files_list = [
-            Path(mfcr_file)
-            for mfcr_file in self.data_location
-            if 'state_deficit' in mfcr_file
-        ]
-        for curr_file in files_list:
-            self.excel_handler.read_data_csv(
-                source='{0}{1}'.format(input_path[:-9], curr_file),
-                has_header=False,
-                encoding='utf8-lossy',
-                missing_utf8_is_empty_string=True,
-            )
-            self.rename_and_drop_columns(
-            ).clean_data(
-            ).get_last_observation_and_stack_it(
-            )
-
-    def clean_up_state_deficit_final(self) -> 'MFCRStateDeficitData':
-        """Get the last observation of the dataframe.
-
-        Get the month of the observation.
-        Get the correct year of the observation:
-        note that for december the data is always
-        published in January.
-        Rearrange the columns.
-
-        Returns:
-            MFCRStateDeficitData: An instance of the MFCRStateDeficitData
-            class.
-        """
-        self.state_deficit_final = (self.state_deficit_final.with_columns(
-            pl.when(
-                pl.col('month') == 'Prosinec',
-            ).then(
-                pl.col('datum').dt.year() - 1,
-            ).otherwise(
-                pl.col('datum').dt.year(),
-            ).alias(
-                'year',
-            )).select(
-                ['datum', 'year', 'month', 'saldo'],
-            ))
         return self
 
     def run_it_all(self):
         """Execute all the steps to process the MFCRStateDeficitData data.
 
         This method performs the following steps:
-        Finds all csvs of the 'state_deficit' typ.
-        Renames and drops some columns.
-        Removes non-breaking spaces from all strings.
-        Takes the last row and binds it to self.state_deficit_final.
-        Cleans up the self.state_deficit_final and saves the output.
+        Renames all and drops some columns.
+        Removes non-breaking spaces from all strings and changes datatypes
+        of 'saldo' and 'date_published' columns.
+        Leaves the last row (available observation) for each year only.
+        Rearranges columns.
+
         Returns:
         MFCRStateDeficitData: An instance of the MFCRStateDeficitData
         class.
         """
-        self.process_all_csvs()
-        self.clean_up_state_deficit_final()
-        self.excel_handler.df = self.state_deficit_final
+        self.excel_handler.read_data_csv(
+            source=self.input_file,
+            has_header=True,
+            encoding='utf8-lossy',
+            missing_utf8_is_empty_string=True,
+        )
+        self.rename_and_drop_columns(
+        ).clean_data(
+        ).leave_last_rows()
         self.excel_handler.write_data(output_file=self.output_file)
 
 
